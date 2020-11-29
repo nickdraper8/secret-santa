7f82e6ade8aaf014ca94698e22556e497ffd1e4b
nickdraper <nickdraper8@gmail.com>
2020-11-28 19:17:44 -0500

    done some work on the navbar

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import "./nav.css";
+
+function Nav() {
+    return(
+        <nav>
+            <h2>Created by Nick Draper</h2>
+            <h1>Secret Santa</h1>
+            <div id="nav-links">
+                <a href="https://github.com/nickdraper8"><i className="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/nicholas-draper/"><i className="fab fa-linkedin"></i></a>
+                <a href="https://nickdraper8.github.io/portfolio-page/"><i class="fas fa-address-card"></i></a>
+            </div>
+        </nav>    
+    )
+};
+
+export default Nav;
