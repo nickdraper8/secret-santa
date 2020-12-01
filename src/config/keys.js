aa2c789d72d512c6e44eb249a5986ad46b7e85dd
nickdraper <nickdraper8@gmail.com>
2020-12-01 14:33:24 -0500

    added env file and hopefuly that will work

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,11 @@
 console.log(`you are running --${process.env.NODE_ENV}-- mode`)
+const { REACT_APP_TWILIO_KEY } = process.env;
+console.log(`react_app_twilio_key from .env: ${REACT_APP_TWILIO_KEY}`);
+
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./keys_prod");
+  module.exports = {
+    twilioKey: REACT_APP_TWILIO_KEY
+  };
 } else {
   module.exports = require("./keys_dev");
 }
