834f9865ab57a9a131f12b13c7a5fce6a3cc9a48
nickdraper <nickdraper8@gmail.com>
2020-12-01 10:29:32 -0500

    added twilio api call and set up keys

---
+++ src/util/twilio_util.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const keys = require('../config/keys');
+
+export const sendMessage = (giftGiver, giftReciever, giftGiverNumber) => {
+
+    let message = `Hey ${giftGiver}, pssstt! This is Nick Draper's Secret Santa App letting you know that you will be buying a gift for ${giftReciever}. Happy Holidays!`
+
+    return axios({
+        "method":"POST",
+        "url":`https://twilio-sms.p.rapidapi.com/2010-04-01/Accounts/a/Messages.json`,
+        "headers":{
+        "content-type":"application/json",
+        "x-rapidapi-host":"twilio-sms.p.rapidapi.com",
+        "x-rapidapi-key":keys.twilioKey,
+        "useQueryString":true
+        },"params":{
+        "from":"+19093651823",
+        "body":message,
+        "to":`+1${giftGiverNumber}`
+        }, "data": {}
+        })
+        .then((res)=>{
+        console.log(res)
+        })
+        .catch((error) => console.log( error )
+        )
+}
+
